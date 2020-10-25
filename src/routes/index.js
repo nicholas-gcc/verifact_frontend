6bf091bbbd8956b82c1e895ce954b3880437b35d
jtan381 <jtan381@e.ntu.edu.sg>
2020-10-25 17:27:15 +0800

    rename LandingPage-> Home

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import LandingPage from "../views/LandingPage";
+import Home from "../views/Home";
 import NotFound from "../views/404";
 
 export default [
-  <Route exact key="home" path="/" component={LandingPage} />,
+  <Route exact key="home" path="/" component={Home} />,
   <Route key="404" path="*" component={NotFound} />,
 ];

5ee2f78fd3bd57b4d3c5dc9d9dc0dd0bd128024f
jtan381 <jtan381@e.ntu.edu.sg>
2020-10-25 16:52:46 +0800

    simulate retriving object from getQuestionById

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,15 +1,19 @@
 import React, { useEffect, useState } from "react";
 import OpenGraphMeta from "./OpenGraphMeta"
-import { Button } from "react-bootstrap";
-import { IoMdArrowDropup } from "react-icons/io";
 import styled from "styled-components";
 import { useHistory } from "react-router-dom";
 
 const getQuestionById = (question_id) => {
   /* TODO: Get question data via question ID
      Expect return result: dict() */
-
-  // retrun {up_votes:"", username:"", ....}
+  const result = {
+    date_created: "July 1, 2020",
+    text: "Is it true that everything happens for a reason?",
+    news_url: "https://www.notion.so/Contentforslides28bb12e73c124253b1513f5aa4cb1ec3",
+    news_title: "Is it true that everything happens for a reason? Is it true that everything happens for a reason? Is it true that everything happens for a reason?",
+    news_image: "https://media.proprofs.com/images/QM/user_images/2503852/1568196428.jpg"
+  }
+  return result
 }
 
 const clickhandle = (history, question_id) => {
@@ -18,56 +22,23 @@ const clickhandle = (history, question_id) => {
 }
 
 export default function QuestionCard(props) {
-  const [data, setData] = useState({ up_votes: "12", username: "" })
+  const [data, setData] = useState({ date_created: "TBC", text: "", news_url: "", news_title: "", news_image: "" })
   let history = useHistory();
-  const { question_id, visual } = props
-  const up_votes = "1100"
-  const username = "Mr. ABC"
-  const question = "Is it true that everything happens for a reason?"
-  const media_url = "https://www.notion.so/Contentforslides28bb12e73c124253b1513f5aa4cb1ec3"
-  const media_title = "Is it true that everything happens for a reason? Is it true that everything happens for a reason? Is it true that everything happens for a reason?"
-  const media_image = "https://media.proprofs.com/images/QM/user_images/2503852/1568196428.jpg"
-  const date_created = "July 1, 2020"
-  const nums_answer = "42"
-  const nums_true = "32"
-  const nums_false = "10"
+  const { question_id } = props
 
   useEffect(() => {
     setData(getQuestionById(question_id))
   }, [question_id])
 
   return <Wrapper onClick={() => clickhandle(history, question_id)} style={{ cursor: 'pointer' }}>
-    <div style={{ paddingRight: '20px' }}>
-      <CustomButton variant="warning" ><IoMdArrowDropup style={{ fontSize: '30px' }} /><div>{up_votes}</div></CustomButton>
-    </div>
     <div style={{ paddingLeft: "20px", marginBottom: '15px' }}>
-      <h1 style={{ fontSize: '25px', fontWeight: 'bold' }}>{question}</h1>
-      <p style={{ fontWeight: 'bold' }}>{nums_answer} answers ({nums_true}true, {nums_false}false)</p>
-
-      <OpenGraphMeta media_url={media_url} media_title={media_title} media_image={media_image} />
-      {visual ? (<>
-        <ContentWrapper>
-          <p >Asked by <b>{username}</b> on {date_created}</p>
-
-        </ContentWrapper></>) : null}
+      <h1 style={{ fontSize: '25px', fontWeight: 'bold' }}>{data.text}</h1>
+      <OpenGraphMeta media_url={data.news_url} media_title={data.news_title} media_image={data.news_image} />
     </div>
   </Wrapper>
 }
 
 const Wrapper = styled.div`
   display: grid;
-  grid-template-columns: 85px  auto;
   padding-top: 60px;
-`;
-
-const ContentWrapper = styled.div`
-  display: grid;
-  grid-template-columns: auto auto;
-`;
-
-const CustomButton = styled(Button)`
-  height: 55px;
-  width: 55px;
-  padding: 0px;
-  border-radius: 10px;
 `;
